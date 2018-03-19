22f79220657b40caf0ddcacf097caf0104ef1202
matilbella <dikethelma55@gmail.com>
2018-03-19 19:20:54 +0100

    Sign up

--- resources/views/front/signup.blade.php
+++ resources/views/front/signup.blade.php
@@ -14,7 +14,7 @@
   <!--Font Awesome -->
   <link href="https://use.fontawesome.com/releases/v5.0.6/css/all.css" rel="stylesheet">
   <!--Main CSS-->
-  <link href="https://fonts.googleapis.com/css?family=Ubuntu" rel="stylesheet">
+  <link href="https://fonts.googleapis.com/css?family=Open+Sans" rel="stylesheet">
   <link rel="stylesheet" href="/css/main.css">
 
 
@@ -22,11 +22,82 @@
 </head>
 
 <body>
+  <div class="container">
+    <div class="row">
+      <div class="col-md-4">
+
+      </div>
+
+      <div class="col-md-4">
+        <div class="logo-banner">
+      <img src="" alt="" srcset="">
+      <h5 class="signupheader">
+        CREATE FARMERS PROFILE
+      </h3>
+    </div>
+
+    <div class="signupcontent">      
+        <form class="form">
+
+          <label class="mr-sm-2" for="inlineFormCustomSelect">Business Category</label>
+          <select class="custom-select border-top-0 border-left-0 border-right-0" id="inlineFormCustomSelect">
+            <option selected>Choose...</option>
+            <option value="1">Poultry</option>
+            <option value="2">Foodstuffs</option>
+            <option value="3">Animals</option>
+          </select>
+        
+          <div class="form-group">          
+            <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Email">
+          </div>
+
+          <div class="form-group">          
+            <input type="password" class="form-control" id="exampleInputPassword1" placeholder="Password">
+          </div>
+        
+          <div class="form-group">          
+            <input type="password" class="form-control" id="exampleInputPassword1" placeholder="Password Again">
+          </div>
+
+          <div class="form-group">          
+            <input type="text" class="form-control" id="exampleInputPassword1" placeholder="State">
+          </div>
+
+          <p class="login-text mb-3 text-justify">By clicking Register, You are indicating that you have read and agreed to the <span class="terms">terms and conditions</span> of this service
+
+        
+          <div class="row">
+            <div class="col-md-4">
+              <button type="submit" class="btn btn-default">No,thanks!</button>
+            </div>
+            <div class="col-md-4">
+
+            </div>
+            <div class="col-md-4 text-right">
+              <button type="submit" class="btn btn-primary">Register</button>
+            </div>
+          </div>
+        
+        </form>
+        </div>
+      </div>
+       
+    </div>
+      </div>
+
+      <div class="col-md-4">
+        
+      </div>
+    </div>
+    
+  </div>
+
+
   <script src="/js/jquery/jquery.min.js"></script>
   <script src="/js/botstrap/bootstrap.min.js"></script>
   <script src="/js/items.js"></script>
   <script src="/js/sweetalert2.min.js"></script>
  
-</script>
+</body>
 </html>
  
