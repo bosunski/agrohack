08ec86124eed4a677213e25882cfd438dfef0fda
Edidiong Udombat <nedyudombat@gmail.com>
2018-03-19 16:54:00 +0100

    setup demo html and css

---
+++ resources/views/front/header.blade.php
@@ -0,0 +1,32 @@
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta http-equiv="X-UA-Compatible" content="ie=edge">
+  <title></title>
+
+  <!-- favicon -->
+  <link rel="icon" href="/img/PNG/Icon.png" sizes="16x16" type="image/png">
+  <!--Bootstrap CSS -->
+  <link rel="stylesheet" href="/css/bootstrap/bootstrap.min.css">
+  <!--Font Awesome -->
+  <link href="https://use.fontawesome.com/releases/v5.0.6/css/all.css" rel="stylesheet">
+  <!--Main CSS-->
+  <link href="https://fonts.googleapis.com/css?family=Ubuntu" rel="stylesheet">
+  <link rel="stylesheet" href="/css/custom.css">
+
+
+
+</head>
+
+<body>
+  <script src="/js/jquery/jquery.min.js"></script>
+  <script src="/js/botstrap/bootstrap.min.js"></script>
+  <script src="/js/items.js"></script>
+  <script src="/js/sweetalert2.min.js"></script>
+ 
+</script>
+</html>
+ 
